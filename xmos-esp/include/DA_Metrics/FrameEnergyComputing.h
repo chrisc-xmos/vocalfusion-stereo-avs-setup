a3776f760c0d0f3eaf09ccbbef918963c964b5ba
Chris Clarke <chrisc@xmos.com>
2018-03-20 09:14:07 +0000

    Better documentation in header esp files

--- xmos-esp/include/DA_Metrics/FrameEnergyComputing.h
+++ xmos-esp/include/DA_Metrics/FrameEnergyComputing.h
@@ -1,28 +1,61 @@
+/** @file */
+
 #ifndef FRAMEENERGYCLASS_H
 #define FRAMEENERGYCLASS_H
 
-#define Word16 int16_t
-#define Word64 int64_t
-
+/**
+ * An Amazon AVS SDK compatible interface class to the XMOS Echo Spatial Perception library that 
+ * runs directly on the Vocalfusion device. This class provides a C++ wrapper around the XMOS
+ * lib-esp library specifically to allow direct linking to the Amazon Alexa Voice Service SDK and 
+ * has been tested with version 1.6 of that SDK.
+ */
 class FrameEnergyClass
 {
 public:
-    // constructor with framelength passed
-    FrameEnergyClass(unsigned int);
+    /**
+     * This method invokes I<SUP>2</SUP>C startup code. Calls to other methods before this method 
+     * has been called are likely to yield incorrect results. 
+     * \param frameSize is ignored as the library only supports 8ms frames (128 samples 
+     * at 16k Samples/s).
+     */
+    FrameEnergyClass(unsigned int frameSize);
 
+    /**
+     * This method closes I<SUP>2</SUP>C link. Calls to other methods after the destructor has 
+     * been called are likely to fail or give incorrect results.
+     */
     ~FrameEnergyClass();
 
-    // process function
-    void process(const bool, const Word64);
+    /**
+     * The process function is unused and data sent to it
+     * is discarded. The XMOS ESP software on the vocalfusion device takes energy values 
+     * and voice detection indicators direct from the modified Amazon reference code also 
+     * running on the deivce.
+     * \param GVAD is intended by the Amazon SDK to indicate that voice activity has been 
+     * detected. However, since the XMOS Vocalfusion device calculates voice activity 
+     * directly, this parameter is ignored.
+     * \param currentFrameEnergy is the calculated energy in the current frame of audio 
+     * samples. As the energy is calculated within the XMOS Vocalfusion device's ESP 
+     * processing block, this value is ignored.
+     */
+    void process(const bool GVAD, const Word64 currentFrameEnergy);
 
-    // block reset function
+    /**
+     * The block reset method is ignored by the XMOS VocalFusion board.
+     */
     void blkReset(void);
 
-    // Get Ambient Energy
-    int getAmbientEnergy();
+    /**
+     * Get level of audio energy in the ambient environment from the XMOS VocalFusion board via the I<SUP>2</SUP>C link.
+     * \return The energy value as a 32 bit signed integer. The algorithm used is a statically allocated memory version of the Amazon reference design for Echo Spatial Perception.
+     */
+    int getAmbientEnergy(void);
 
-    // Get Voiced Energy
-    int getVoicedEnergy();
+    /**
+     * Get the energy associated with detected speaking from the XMOS VocalFusion board via the I<SUP>2</SUP>C link.
+     * \return The energy value as a 32 bit signed integer. The algorithm used is a statically allocated memory version of the Amazon reference design for Echo Spatial Perception.
+     */
+    int getVoicedEnergy(void);
 };
 
 #endif // FRAMEENERGYCLASS_H
--- xmos-esp/include/VAD_Features/VAD_class.h
+++ xmos-esp/include/VAD_Features/VAD_class.h
@@ -1,27 +1,49 @@
-/* VAD_class.h
- *
- * Not required on host side in XMOS implementation. Minimal stub set required to make Amazon example compile
- */
+/** @file */
+
 #ifndef VADCLASS_H
 #define VADCLASS_H
 
-
-#define Word16 int16_t
-#define Word64 int64_t
-
+/**
+ *
+ * An Amazon Alexa Voice Service (AVS) SDK compatible interface class to the XMOS Echo Spatial 
+ * Perception (ESP)library that runs directly on the Vocalfusion device. This class provides a 
+ * C++ stub for methods that the Amazon AVS SDK assumes will be present in an ESP implementation. 
+ * Since the XMOS vocalfusion implementation runs on the vocalfusion device rather than the host 
+ * these methods are all stubs and do not cause any operations within the library. They are 
+ * provided only to permit use of the library with an unmodified Amazon AVS SDK and have been 
+ * tested with version 1.6 of that SDK.
+ */
 class VADClass
 {
 public:
 
-    // constructor with framelength passed
-    VADClass(unsigned int);
+    /**
+     * This method does nothing for reasons explained above. 
+     * \param frameSize is ignored as the library only supports 8ms frames (128 samples 
+     * at 16k Samples/s).
+     */
+    VADClass(unsigned int frameSize);
 
+    /**
+     * Destructor. This method does nothing for reasons explained above.
+     */
     ~VADClass();
 
-     // process function
-    void process(Word16*, bool&, Word64);
+    /**
+     * This method does nothing for reasons explained above. In the Amazon AVS implementation,
+     * this method computes the energy contained in the frame and 
+     * \param sampleBuffer a set of signed 16 bit samples to process. The XMOS ESP library 
+     * library does not use these samples as it sources the audio from earlier in the signal
+     * path.
+     * \param voiceActivityDetected this variable is NOT updated by the XMOS ESP library as 
+     * the Amazon AVS SDK (v1.6) uses this solely for updating values within the 
+     * frameEnergyComputing class object which is also implemented on the Vocalfusion device.
+     */
+    void process(Word16* sampleBuffer, bool& voiceActivityDetected, Word64& frameEnergy);
 
-    // VAD block reset function
+    /**
+     * The block reset method is ignored by the XMOS VocalFusion board.
+     */
     void blkReset(void);
 };
 
