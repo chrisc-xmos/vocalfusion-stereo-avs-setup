cc6fa27e8a8093e98113cfadc7ed3d4ae87629b1
Chris Clarke <chrisc@xmos.com>
2018-05-01 15:15:16 +0000

    Added additional required header file

---
+++ xmos-esp/include/common/fixed_pt.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2017 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ *
+ * You may not use this file except in compliance with the terms and conditions set forth in the accompanying LICENSE.TXT file.
+ *
+ * THESE MATERIALS ARE PROVIDED ON AN "AS IS" BASIS.  AMAZON SPECIFICALLY DISCLAIMS, WITH RESPECT TO THESE MATERIALS, ALL
+ * WARRANTIES, EXPRESS, IMPLIED, OR STATUTORY, INCLUDING THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE, AND NON-INFRINGEMENT.
+ */
+
+// Modified by XMOS Ltd. 2018 as permitted by the Amazon licence.
+
+#ifndef FIXED_PT_H
+#define FIXED_PT_H
+
+typedef  short                  Word16;
+typedef  long long              Word64;
+
+#endif // FIXED_PT_H
